3afcf819fccc5904102dd526ab472d90838c219e
TeamStetch <130715050+TeamStetch@users.noreply.github.com>
2023-06-04 17:56:51 +0300

    Update thumbnails.py

--- ShizukaXMusic/utils/thumbnails.py
+++ ShizukaXMusic/utils/thumbnails.py
@@ -81,7 +81,7 @@ async def gen_thumb(videoid):
         para = textwrap.wrap(title, width=32)
         j = 0
         draw.text(
-            (5, 5), f"{BOT_USERNAME}", fill="white", font=name_font
+            (5, 5), "STETCHFY", fill="white", font=name_font
         )
         draw.text(
             (600, 150),
